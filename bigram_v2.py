d75524ccc2947b64430f2fb20a720970e097b70e
Michael Blum <49876653+mab8192@users.noreply.github.com>
2023-02-06 10:34:42 -0700

    added multihead attention, blocks, full transformer decoder

--- bigram_v2.py
+++ bigram_v2.py
@@ -5,12 +5,18 @@ import torch.nn.functional as F
 torch.manual_seed(1337)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-block_size = 8
-batch_size = 32
+print("Using", device)
+block_size = 256
+batch_size = 64
+learning_rate = 6e-4
 max_iters = 3000
 eval_iters = 50
 eval_every = 300
-embed_dim = 32
+
+embed_dim = 128
+n_heads = 4
+n_layers = 4
+dropout = 0.2
 
 # ----- Load and prepare data -----
 with open("input.txt") as f:
@@ -42,12 +48,83 @@ def get_batch(split):
 
 
 # ----- Define Model -----
+class Head(nn.Module):
+    def __init__(self, head_size) -> None:
+        super().__init__()
+        self.head_size = head_size
+        self.key = nn.Linear(embed_dim, head_size, bias=False)
+        self.query = nn.Linear(embed_dim, head_size, bias=False)
+        self.value = nn.Linear(embed_dim, head_size, bias=False)
+        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        B,T,C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+
+        wei = q @ k.transpose(-2, -1) * self.head_size**-0.5
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
+
+        v = self.value(x)
+        out = wei@v
+        return out
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, head_size) -> None:
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(n_heads)])
+        self.proj = nn.Linear(embed_dim, embed_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=2)
+        out = self.dropout(self.proj(out))
+        return out
+
+
+class FF(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(embed_dim, 4*embed_dim),
+            nn.ReLU(),
+            nn.Linear(4*embed_dim, embed_dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Block(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        head_size = embed_dim // n_heads
+        self.sa = MultiHeadAttention(head_size)
+        self.ff = FF()
+        self.ln1 = nn.LayerNorm(embed_dim)
+        self.ln2 = nn.LayerNorm(embed_dim)
+
+    def forward(self, x):
+        x = self.sa(self.ln1(x)) + x
+        x = self.ff(self.ln2(x)) + x
+        return x
+
+
 class BigramLanguageModel(nn.Module):
     def __init__(self) -> None:
         super().__init__()
 
         self.token_embedding_table = nn.Embedding(vocab_size, embed_dim)
         self.position_embedding_table = nn.Embedding(block_size, embed_dim)
+
+        self.blocks = nn.Sequential(*[Block() for _ in range(n_layers)])
+        self.ln_f = nn.LayerNorm(embed_dim)  # Final layer norm
         self.lm_head = nn.Linear(embed_dim, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -57,6 +134,8 @@ class BigramLanguageModel(nn.Module):
         pos_emb = self.position_embedding_table(torch.arange(T, device=device))
 
         x = token_emb + pos_emb
+        x = self.blocks(x)
+        x = self.ln_f(x)
         logits = self.lm_head(x)  # (B, T, vocab_size)
 
         if targets is None:
@@ -71,7 +150,8 @@ class BigramLanguageModel(nn.Module):
 
     def generate(self, idx, max_new_tokens):
         for _ in range (max_new_tokens):
-            logits, loss = self(idx)
+            idx_con = idx[:, -block_size:]  # Truncate to ensure we never pass in more than `block_size` tokens
+            logits, loss = self(idx_con)
             logits = logits[:, -1, :] # Take the last element in the time dimension
             probs = F.softmax(logits, dim=-1)
             idx_next = torch.multinomial(probs, num_samples=1)
@@ -97,7 +177,7 @@ def estimate_loss():
     model.train()
     return out
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
 for step in range(max_iters):
     xb, yb = get_batch('train')
@@ -113,5 +193,6 @@ for step in range(max_iters):
 
 
 # ----- Test Model -----
-inp = torch.zeros(1, 1).long()
+inp = torch.zeros(1, 1).long().to(device)
+model.eval()
 print(decode(model.generate(inp, max_new_tokens=500)[0].tolist()))
